990968f0ee5a770dc2276d595af9503a5a08dd6c
Andrew Epstein <aepstein@udel.edu>
2024-05-17 14:23:25 -0400

    WIP on complement post

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './styles.css';
+import TextToBinary from "./components/TextToBinary";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. (DONE)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+
+    return (
+        <Fragment>
+            <Header/>
+            <div style={{display: 'flex', flex: 1, flexDirection: 'column'}}>
+                <TextToBinary/>
+            </div>
+            <Footer/>
+        </Fragment>
+    );
 }
 
 export default App;
