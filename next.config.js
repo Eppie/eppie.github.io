7b5b786ee235f69eb04b7bb929b6cc188e13ed68
Andrew Epstein <aepstein@udel.edu>
2024-06-10 19:04:20 -0400

    Refactor next.config.js to simplify configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-const withTM = require('next-transpile-modules')(['react-syntax-highlighter']); // Add the package you need to transpile
-const isProd = process.env.NODE_ENV === 'production';
+const withTM = require('next-transpile-modules')(['react-syntax-highlighter']);
+// const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withTM({
-  basePath: isProd ? '/eppie.github.io' : '',
+  // basePath: isProd ? '/eppie.github.io' : '',
   trailingSlash: true,
   output: 'export',
 });
